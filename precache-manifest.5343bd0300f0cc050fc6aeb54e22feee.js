41dc838f6b9fc2e367c3140caacba7d70f1dddc3
Joel Gaeta <joelsgaeta@gmail.com>
2020-01-06 20:53:39 -0500

    Updates

---
+++ precache-manifest.5343bd0300f0cc050fc6aeb54e22feee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa83dea93760e4732466857368c553b2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7c5121d91d9cf16d30bf",
+    "url": "/react-gh-pages/static/css/main.4018f348.chunk.css"
+  },
+  {
+    "revision": "4118dd12936beeaa46e9",
+    "url": "/react-gh-pages/static/js/2.690ba5d0.chunk.js"
+  },
+  {
+    "revision": "7c5121d91d9cf16d30bf",
+    "url": "/react-gh-pages/static/js/main.48fea451.chunk.js"
+  },
+  {
+    "revision": "9c55756e959abf4af8d4",
+    "url": "/react-gh-pages/static/js/runtime-main.ea1da96f.js"
+  }
+]);
